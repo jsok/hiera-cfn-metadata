3670c792f87a4544eecd9189cc5098e525b60231
Jonathan Sokolowski <jsok@expert360.com>
2015-07-01 15:32:24 +1000

    Do not nest CfnV1 signer module

--- lib/aws-sdk-core/signers/cfn_v1.rb
+++ lib/aws-sdk-core/signers/cfn_v1.rb
@@ -3,26 +3,24 @@ require "openssl"
 
 module Aws
   module Signers
-    module Cfn
-      class CfnV1 < Base
-        def sign(http_request)
-          http_request.headers["Authorization"] = authorization
-          http_request
-        end
+    class CfnV1 < Base
+      def sign(http_request)
+        http_request.headers["Authorization"] = authorization()
+        http_request
+      end
 
-        private
+      private
 
-        def authorization
-          return "CFN_V1 #{document}:#{signature}"
-        end
+      def authorization
+        return "CFN_V1 #{document}:#{signature}"
+      end
 
-        def document
-          @credentials.access_key_id
-        end
+      def document
+        @credentials.access_key_id
+      end
 
-        def signature
-          @credentials.secret_access_key
-        end
+      def signature
+        @credentials.secret_access_key
       end
     end
   end
